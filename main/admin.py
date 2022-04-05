7501f7cbbd902fe7dbd1b3033f700a43f437291c
Pragnesh Katkar <pragneshpkatkar374@gmail.com>
2022-04-06 00:38:46 +0530

    Index page directed to dashboard

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from main.models import *
+
 # Register your models here.
+admin.site.register(Prediction)
+admin.site.register(Doctor)
+admin.site.register(DietBlog)
